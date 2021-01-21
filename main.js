5a4e7c0ac43efa1a47e8876f56f28181d47de983
EpiqPhale <james.wedum@gmail.com>
2021-01-20 22:20:28 -0800

    Added sprites and background images

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
-console.log("This was a triumph.");
+ASSET_MANAGER.queueDownload("./sprites/beardo.png");
+ASSET_MANAGER.queueDownload("./bg/foreground.png");
+ASSET_MANAGER.queueDownload("./bg/midground.png");
+ASSET_MANAGER.queueDownload("./bg/midground_far.png");
+ASSET_MANAGER.queueDownload("./bg/background.png");
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
